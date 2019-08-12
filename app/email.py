0115375e873cffa53380625134ad1417beea02ad
Janice-M <janiceink001@gmail.com>
2019-08-13 00:09:19 +0300

    changes in main/views.py  && main/forms.py

--- app/email.py
+++ app/email.py
@@ -10,3 +10,5 @@ def mail_message(subject,template,to,**kwargs):
     email.body= render_template(template + ".txt",**kwargs)
     email.html = render_template(template + ".html",**kwargs)
     mail.send(email)
+    
+    ''' comment  '''
