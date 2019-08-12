851aef339a20b2dc48842abaa5f3c569258dcbc4
Janice-M <janiceink001@gmail.com>
2019-08-13 00:11:28 +0300

    installed datetime

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from . import db
 from . import login_manager
 from flask_login import UserMixin, current_user
 from werkzeug.security import generate_password_hash,check_password_hash
-
+from datetime import datetime
 
 
 @login_manager.user_loader
